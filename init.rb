d9a6b63f1c29ea35f1c9c3c1fd27b49d044fb45e
Thomas Kadauke <tkadauke@imedo.de>
2010-08-10 13:22:01 +0200

    fix gem loading as gem plugin in rails 2.3

--- init.rb
+++ init.rb
@@ -1,13 +1,1 @@
-require File.dirname(__FILE__) + '/lib/background'
-require File.dirname(__FILE__) + '/lib/core_ext/object'
-require File.dirname(__FILE__) + '/lib/core_ext/class'
-require File.dirname(__FILE__) + '/lib/core_ext/numeric'
-require File.dirname(__FILE__) + '/lib/core_ext/symbol'
-require File.dirname(__FILE__) + '/lib/core_ext/nil_class'
-Dir.glob(File.dirname(__FILE__) + '/lib/core_ext/handlers/*.rb').each do |handler|
-  require handler
-end
-Dir.glob(File.dirname(__FILE__) + '/lib/core_ext/error_reporters/*.rb').each do |reporter|
-  require reporter
-end
-require File.dirname(__FILE__) + '/lib/rails_ext/activerecord/base'
+require File.dirname(__FILE__) + '/lib/background_lite'
